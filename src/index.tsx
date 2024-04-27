5f4141b860804d62df318cd74fd1381b1a700211
g.cesaroni <giorgio.cesaroni@me.com>
2024-04-27 18:23:18 +0200

    Updated basename

--- src/index.tsx
+++ src/index.tsx
@@ -9,33 +9,38 @@ import { TasksPage } from "routes/tasks-page";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+   [
+      {
+         path: "/",
+         element: <Layout />,
+         children: [
+            {
+               path: "/",
+               element: <Navigate to="/positioning" />,
+            },
+            {
+               path: "/positioning",
+               element: <PositioningPage />,
+            },
+            {
+               path: "/tasks",
+               element: <TasksPage />,
+            },
+            {
+               path: "/products",
+               element: <ProductsPage />,
+            },
+            {
+               path: "/products/:id",
+               element: <ProductPage />,
+            },
+         ],
+      },
+   ],
    {
-      path: "/",
-      element: <Layout />,
-      children: [
-         {
-            path: "/",
-            element: <Navigate to="/positioning" />,
-         },
-         {
-            path: "/positioning",
-            element: <PositioningPage />,
-         },
-         {
-            path: "/tasks",
-            element: <TasksPage />,
-         },
-         {
-            path: "/products",
-            element: <ProductsPage />,
-         },
-         {
-            path: "/products/:id",
-            element: <ProductPage />,
-         },
-      ],
-   },
-]);
+      basename: "/frontend",
+   }
+);
 
 root.render(<RouterProvider router={router} />);
